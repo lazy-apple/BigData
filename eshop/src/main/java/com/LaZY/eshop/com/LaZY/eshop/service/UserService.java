d0a2ebba9386c8c50873efc15978c7bc6c2c02e3
LaZY <18846171630@163.com>
2019-03-04 21:32:40 +0800

    Project add:service：用户service接口和实现类

---
+++ eshop/src/main/java/com/LaZY/eshop/com/LaZY/eshop/service/UserService.java
@@ -0,0 +1,10 @@
+package com.LaZY.eshop.com.LaZY.eshop.service;
+
+import com.LaZY.eshop.model.User;
+
+/**
+ * @author LaZY(李志一)
+ * @create 2019-03-04 21:20
+ */
+public interface UserService extends BaseService<User>{
+}
---
+++ eshop/src/main/java/com/LaZY/eshop/com/LaZY/eshop/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.LaZY.eshop.com.LaZY.eshop.service.impl;
+
+import com.LaZY.eshop.com.LaZY.eshop.service.BaseService;
+import com.LaZY.eshop.com.LaZY.eshop.service.UserService;
+import com.LaZY.eshop.model.User;
+
+import java.util.List;
+
+/**
+ * @author LaZY(李志一)
+ * @create 2019-03-04 21:20
+ */
+public class UserServiceImpl extends BaseServiceImpl<User> implements UserService{
+
+}
